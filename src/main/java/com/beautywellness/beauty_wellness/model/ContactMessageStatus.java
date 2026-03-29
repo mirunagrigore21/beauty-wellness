2309fcbb9f3d5d930b8ea5290c48b5184e5e536f
Grigore Miruna <grigore.miruna.21@gmail.com>
2026-03-29 19:48:29 +0300

    Update final backend

--- src/main/java/com/beautywellness/beauty_wellness/model/ContactMessageStatus.java
+++ src/main/java/com/beautywellness/beauty_wellness/model/ContactMessageStatus.java
@@ -1,6 +1,6 @@
 package com.beautywellness.beauty_wellness.model;
 
-//statusul unui mesaj trimis din formularul de contact
+//starea unui mesaj trimis din sectiunea de contact
 public enum ContactMessageStatus {
     NEW,
     READ,
